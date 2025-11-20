d6e0a700ac3d7d18935c9d9f8e1443f95c955e21
Ruggery <ruggeryalves@hotmail.com>
2025-11-20 13:27:10 +0000

    Update database.js
    Inserting the connection to mysql and checking if it has been connected.

--- database.js
+++ database.js
@@ -1,1 +1,23 @@
 //(This will handle the Database connection)
+//First we need to import the library
+const mysql = require('mysql2');
+
+//Second we need to create the connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pass1234!',
+    database: 'project_ca2'
+});
+
+//third, we need to try if the connection works
+db.connect((err) => {
+    if (err){
+        console.error('Error while trying to connect to database: ', err);
+    } else{
+        console.log('Database has been connected.');
+    }
+});
+
+//last, export the module
+module.exports = db;
